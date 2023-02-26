6435a3833cdf73e1d779de6cf20e369dc9342aeb
João Pedro <124743136+j0pinha@users.noreply.github.com>
2023-02-25 23:48:07 -0300

    Add files via upload

---
+++ desafio 1.py
@@ -0,0 +1,4 @@
+dia=input('Dia:')
+mes=input('Mês:')
+ano=input('Ano:')
+print('Então você nasceu no',dia,'de',mes,'em',ano,)
---
+++ desafio 2.py
@@ -0,0 +1,4 @@
+n1=int(input('Digite um numero inteiro para ser somado'))
+n2=int(input('Digite outro numero inteiro'))
+soma= n1+n2
+print(soma)
